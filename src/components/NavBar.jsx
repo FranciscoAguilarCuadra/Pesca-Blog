3e967644cb7e3d072697e36906f98cfc2c9e21b3
FranciscoAlumnoUbb <francisco.aguilar1801@alumnos.ubiobio.cl>
2026-04-05 00:32:29 -0400

    Feat: Componente de NavBar agregado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import '../styles/NavBar.css'
 
 export function NavBar() {
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+  const isLoggedIn = !!token
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
+
   return (
-    <nav className="navBar">
-      <Link to="/">Inicio</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/quienes-somos">Quiénes Somos</Link>
+    <nav className="navbar">
+      <div className="navbar__left">
+        <Link to="/">Inicio</Link>
+      </div>
+
+      <div className="navbar__right">
+        {isLoggedIn ? (
+          <button className='btn-logout' onClick={handleLogout}>Cerrar sesión</button>
+        ) : (
+          <Link to="/login">Ingresar</Link>
+        )}
+      </div>
     </nav>
   )
 }
